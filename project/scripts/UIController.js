56adaf33a337e4e985e97a681766bd5b694ed925
BigBrosh <30599468+BigBrosh@users.noreply.github.com>
2018-03-16 09:14:08 +0200

    arrows control modified

--- project/scripts/UIController.js
+++ project/scripts/UIController.js
@@ -2,15 +2,22 @@ export class UIController {
 	constructor (context) {
 		this.context = context;
 		document.addEventListener('keydown', e => {
-			this.leftArrow(e);
+			this.arrowControll(e);
 		});
+	}
+
+	arrowControll(e) {
+		this.leftArrow(e);
+		this.rightArrow(e);
 	}
 
 	leftArrow(e) {
 		if (e.keyCode === 37)
-		{
 			this.context.figures.offsetX--;
-			console.log(this.context.figures.offsetX);
-		}
+	}
+
+	rightArrow(e) {
+		if (e.keyCode === 39)
+			this.context.figures.offsetX++;
 	}
 }
