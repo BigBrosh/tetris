33af47eb192d411ee1a11701a4fb736057fb7655
BigBrosh <30599468+BigBrosh@users.noreply.github.com>
2018-03-17 19:23:03 +0200

    left side checker added

--- project/scripts/UIController.js
+++ project/scripts/UIController.js
@@ -17,7 +17,8 @@ export class UIController {
 	leftArrow(e) {
 		if (e.keyCode === 37)
 		{
-			if (this.figures.offsetX !== 0)
+			if (this.figures.offsetX !== 0 &&
+				this.leftSideChecker())
 				this.figures.offsetX--;
 		}
 	}
@@ -28,6 +29,15 @@ export class UIController {
 			if (this.figures[this.figures.currentName][0].length + this.figures.offsetX < this.context.field[0].length && this.rightSideChecker())
 				this.figures.offsetX++;
 		}
+	}
+
+	leftSideChecker() {
+		for (let row = 0; row < this.figures[this.figures.currentName].length; row++) {
+			if (this.figures[this.figures.currentName][row][0] !== 0 &&
+				this.context.field[row + this.figures.offsetY][this.figures.offsetX - 1] !== 0)
+				return false;
+		}
+		return true;
 	}
 
 	rightSideChecker() {
