0e38a125e0982e595674a90b0f28abc17acc989b
BigBrosh <30599468+BigBrosh@users.noreply.github.com>
2018-03-18 15:32:33 +0200

    rotating added (need to modify)

--- project/scripts/UIController.js
+++ project/scripts/UIController.js
@@ -12,6 +12,8 @@ export class UIController {
 		this.leftArrow(e);
 		this.rightArrow(e);
 		this.downArrow(e);
+		this.rotateLeft(e);
+		this.rotateRight(e);
 	}
 
 	leftArrow(e) {
@@ -22,6 +24,21 @@ export class UIController {
 	rightArrow(e) {
 		if (e.keyCode === 39 && this.sideChecker(1))
 			this.figures.offsetX++;
+	}
+
+	downArrow(e) {
+		if (e.keyCode === 40)
+			this.figures.offsetY++;	
+	}
+
+	rotateLeft(e) {
+		if (e.keyCode === 81)
+			this.rotateFigure(0)
+	}
+
+	rotateRight(e) {
+		if (e.keyCode === 69)
+			this.rotateFigure(1)
 	}
 
 	leftSideChecker() {
@@ -50,8 +67,31 @@ export class UIController {
 		return checker;
 	}
 
-	downArrow(e) {
-		if (e.keyCode === 40)
-			this.figures.offsetY++;	
+	rotateFigure(direction) {
+		let figure = this.figures.currentFigure;
+
+		for (let y = 0; y < figure.length; ++y) {
+			for (let x = 0; x < y; ++x) {
+				[
+					figure[y][x],
+					figure[x][y]
+				] = [
+					figure[x][y],
+					figure[y][x]
+				]
+			}
+		}
+
+		if (direction === 1)
+		{
+			figure.forEach((row) => {
+				row.reverse();
+			})
+		}
+
+		else if (direction === 0)
+		{
+			figure.reverse();
+		}
 	}
 }
